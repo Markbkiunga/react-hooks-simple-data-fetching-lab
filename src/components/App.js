b0f559a7a21e71809d7091fdb912ef7bbba82d9e
Mark Brian <markbkiunga2017@gmail.com>
2024-08-17 14:34:06 +0300

    Completes Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 // create your App component here
+import { useEffect, useState } from 'react';
+import React from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((dog) => {
+        setDogImage(dog.message);
+        setIsLoaded(true);
+        console.log(dog.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoaded ? <img src={dogImage} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
